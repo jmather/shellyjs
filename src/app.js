24727d2124be017fb267a94a247d750438bdedc8
Scott Dale <scott@lgdales.com>
2013-02-25 17:17:40 +1100

    clean up dead code;

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@ server.use(function(req, res, next) {
 	var psession = req.params.session;
 	// SWD - should grab user object out of check and stuff into req.session
 	if(!session.check(psession)) {
-//		var wrapper = shutil.wrapper(cmd, psession, 1, {info: "bad session token"});
 		var event = shutil.event("event.error", {info: 'bad session token'});
 		res.send(event);
 		return 0;
@@ -81,7 +80,6 @@ server.use(function(req, res, next) {
 	// make sure we have a user object as we are passed session check
 	user.loadOrCreate(req.session.uid, function(error, data) {
 		if(error != 0) {
-//			var wrapper = shutil.wrapper(cmd, psession, 2, {info: "unable to load user: " + req.session.uid});
 			var event = shutil.event("event.error", {info: "unable to load user: " + req.session.uid});
 			res.send(event);
 			return 0;
