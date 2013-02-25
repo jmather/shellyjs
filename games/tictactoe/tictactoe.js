1c18c4209ca5d38fab85663923a45926f81c4b05
Scott Dale <scott@lgdales.com>
2013-02-25 18:25:10 +1100

    display the winning squares in green;

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -16,6 +16,8 @@ tictactoe.init = function(req, cb)
 	req.env.game.state.gameBoard = gameBoard;  // SWD: change this to just board
 	// first player is always X
 	req.env.game.state.xes = req.session.uid;
+	req.env.game.state.winner = 0;
+	req.env.game.state.winnerSet = null;
 	
 	cb(0, shutil.event("event.game.info", req.env.game));
 }
@@ -47,12 +49,14 @@ function checkWin(gb) {
 		if(gb[i][0] == gb[i][1] && gb[i][0] == gb[i][2]) {
 			if(gb[i][0]!='') {
 				res.winner = gb[i][0];
+				res.set = ['x'+i+'y0', 'x'+i+'y1', 'x'+i+'y2'];
 				return res;			
 			}
 		}
 		if(gb[0][i] == gb[1][i] && gb[0][i] == gb[2][i]) {
 			if(gb[0][i] != '') {
 				res.winner = gb[0][i];
+				res.set = ['x0y'+i, 'x1y'+i, 'x2y'+i];
 				return res;
 			}
 		}
@@ -61,12 +65,14 @@ function checkWin(gb) {
 	if(gb[0][0]==gb[1][1] && gb[0][0]==gb[2][2]) {
 		if(gb[1][1] != '') {
 			res.winner = gb[1][1];
+			res.set = ['x0y0', 'x1y1', 'x2y2'];			
 			return res;
 		}
 	}
 	if(gb[0][2]==gb[1][1] && gb[0][2]==gb[2][0]) {
 		if(gb[1][1] != '') {
 			res.winner = gb[1][1];
+			res.set = ['x0y2', 'x1y1', 'x2y0'];			
 			return res;
 		}
 	}
@@ -95,16 +101,18 @@ tictactoe.turn = function(req, cb)
 	var win = checkWin(gameBoard);
 	if(win.winner != '') {
 		game.status = "over";
-		game.winner = uid;
 		game.whoTurn = 0;
+		game.state.winner = uid;
+		game.state.winnerSet = win.set;
 		cb(0, shutil.event('event.game.over', game));
 		return;
 	}
 	
 	if(checkFull(gameBoard)) {
 		game.status = "over";
-		game.winner = 0;
 		game.whoTurn = 0;
+		game.state.winner = 0;
+		game.state.winnerSet = null;		
 		cb(0, shutil.event('event.game.over', game));
 		return;
 	}
