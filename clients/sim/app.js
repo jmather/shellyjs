f5a2e7e50fbe2c997a2b08739cb883a8aba2fac8
Scott Dale <scott@lgdales.com>
2013-03-02 10:17:44 +1100

    limit log display to 500 lines; clean up dead code and console.log in sim;

--- clients/sim/app.js
+++ clients/sim/app.js
@@ -9,9 +9,9 @@ console.log("go");
 var reqTpl1 = {session: '1:33:xxxx:0', gameId: "193"}
 var reqTpl2 = {session: '1:44:xxxx:0', gameId: "193"}
 
-var gMoveDelay = 200;
+var gMoveDelay = 100;
 
-var gRuns = 500;
+var gRuns = 10000;
 var gMoveSet = [
 						 [0,0], [0,1], [0,2],
 						 [1,0], [1,1], [1,2],
@@ -24,20 +24,11 @@ var gRunCount = 0;
 run();
 function run()
 {
-	console.log(gMoves.length);
-
 	console.log("------------", gRunCount);
 	async.waterfall([
     function(cb){reset(cb);},
     function(cb){join(cb);},
     function(cb){moveUntilEnd(reqTpl1, gMoves, cb);},
-/*
-    function(cb){move(reqTpl1, 0, 0, cb);},
-    function(cb){move(reqTpl2, 1, 0, cb);},
-    function(cb){move(reqTpl1, 0, 1, cb);},
-    function(cb){move(reqTpl2, 1, 1, cb);},
-    function(cb){move(reqTpl1, 0, 2, cb);},
-    */
 	], function (err, result) {
 		gMoves = _.clone(gMoveSet);
 
@@ -59,7 +50,7 @@ function reset(cb)
 	  .send(reqData)
 	  .set('Accept', 'application/json')
 	  .end(function(res){
-			console.log("reset done");
+//			console.log("reset done");
 //			console.log(res.body);
 			cb();
 	  });
@@ -73,7 +64,7 @@ function join(cb)
 	  .send(reqData)
 	  .set('Accept', 'application/json')
 	  .end(function(res){
-			console.log("join done");
+//			console.log("join done");
 //			console.log(res.body);
 			cb();
 	  });
@@ -83,11 +74,6 @@ function join(cb)
 function moveUntilEnd(tpl, moves, cb) {
 	var idx = _.random(moves.length-1);
 	var newMove = moves.splice(idx, 1);
-	console.log("moves", moves, idx);
-	console.log(newMove);
-//	var movesLeft = moves.slice(idx, idx);
-//	console.log("newMove", newMove);
-//	console.log(newMove[0][0], newMove[0][1]);
 	move2(tpl, newMove[0][0], newMove[0][1], function(res) {
 		// if win stop
 		if(res.event == "event.game.over") {
@@ -95,7 +81,6 @@ function moveUntilEnd(tpl, moves, cb) {
 			cb();
 			return;
 		}
-		console.log("whoTurn", res.data.whoTurn);
 		var tpl = (res.data.whoTurn=="44" ? reqTpl2 : reqTpl1 );
 		moveUntilEnd(tpl, moves, cb);
 	});
@@ -103,6 +88,7 @@ function moveUntilEnd(tpl, moves, cb) {
 
 function move2(tpl, x, y, cb)
 {
+	console.log("move:", x, y);
 	var reqData = _.clone(tpl);
 	reqData.cmd = 'game.turn';
 	reqData.move = {x:x, y:y}
@@ -110,30 +96,10 @@ function move2(tpl, x, y, cb)
 	  .send(reqData)
 	  .set('Accept', 'application/json')
 	  .end(function(res){
-			console.log("move done", x, y);
-			console.log(JSON.stringify(res.body));
 			if(gMoveDelay == 0 ) {
 				cb(res.body);
 			} else {
 				setTimeout(function() {cb(res.body)}, gMoveDelay);
-			}
-	  });
-}
-
-function move(tpl, x, y, cb)
-{
-	var reqData = _.clone(tpl);
-	reqData.cmd = 'game.turn';
-	reqData.move = {x:x, y:y}
-	request.post(host+'/api')
-	  .send(reqData)
-	  .set('Accept', 'application/json')
-	  .end(function(res){
-			console.log("move done", x, y);
-			if(gMoveDelay == 0 ) {
-				cb();
-			} else {
-				setTimeout(cb, gMoveDelay);
 			}
 	  });
 }
