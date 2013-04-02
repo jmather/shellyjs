9aadfcace05da1a580f5bd52ed3bb9b6e81370f2
Scott Dale <scott@lgdales.com>
2013-04-03 10:48:37 +1100

    more cleanup from games/admin split;

--- admin/login/login.js
+++ admin/login/login.js
@@ -1,27 +1,3 @@
-function setCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function deleteCookie(name) {
-    setCookie(name,"",-1);
-}
 
 function setToken(token) {
   Env.shToken = token;
@@ -61,102 +37,11 @@ function doLogin() {
   })
 }
 
-function doRegister() {
-  hideAllMessages();
-
-  if ($("#pass").val() !== $("#passVerify").val()) {
-    error("password do not match");
-    return;
-  }
-
-  var data = {cmd: "reg.create",
-    email: $("#email").val(),
-    password: $("#pass").val()
-  }
-
-  $("#signInLoading").css("display","block");
-  $.ajax ({
-    type: "POST",
-    url: Env.restUrl,
-    async: false,
-    dataType: "json",
-    data: JSON.stringify(data),
-    success: function (res, status) {
-      $("#signInLoading").css("display","none");
-      debug.info(res);
-      if (res.event === "event.error") {
-        error(res.message);
-      } else {
-        $.cookie("shSession", res.data.session, {path: '/', expires: 365});
-        window.location.href = "/index.html";
-      }
-    },
-    error: function (xhr, status, err) {
-      error(err);
-    }
-  })
-}
-
-function doAnonymous() {
-  hideAllMessages();
-
-  var data = {cmd: "reg.anonymous",
-    token: Env.shToken
-  }
-  console.log(data);
-
-  $("#signInLoading").css("display","block");
-  $.ajax ({
-    type: "POST",
-    url: Env.restUrl,
-    async: false,
-    dataType: "json",
-    data: JSON.stringify(data),
-    success: function (res, status) {
-      $("#signInLoading").css("display","none");
-      debug.info(res);
-      if (res.event === "event.error") {
-        if (res.code === "user_upgraded") {
-          $("#upgradeDiv").css("display", "block");
-        } else {
-          error(res.message);
-        }
-      } else {
-        $.cookie("shSession", res.data.session, {path: '/', expires: 365});
-        window.location.href = "/index.html";
-      }
-    },
-    error: function (xhr, status, err) {
-      error(err);
-    }
-  })
-}
-
-function registerMode()
-{
-  $("#shSubTitle").text("Register");
-	hideAllMessages();
-  $(".loginConfig").css("display", "none");
-  $(".registerConfig").css("display", "block");
-  $(".registerRow").css("display", "");
-}
-
-function loginMode()
-{
-  $("#shSubTitle").text("Login");
-
-	hideAllMessages();
-  $(".loginConfig").css("display", "block");
-  $(".registerConfig").css("display", "none");
-  $(".registerRow").css("display", "none");
-}
-
 function hideAllMessages()
 {
   $("#messageDiv").css("display", "none");
   $("#signInInfo").css("display", "none");
 	$("#signInError").css("display", "none");
-  $("#upgradeDiv").css("display", "none");
   $("#signInLoading").css("display","none");
 }
 
--- www/games/login/login.js
+++ www/games/login/login.js
@@ -1,27 +1,3 @@
-function setCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function deleteCookie(name) {
-    setCookie(name,"",-1);
-}
 
 function setToken(token) {
   Env.shToken = token;
@@ -52,7 +28,7 @@ function doLogin() {
         error(res.message);
       } else {
         $.cookie("shSession", res.data.session, {path: '/', expires: 365});
-        window.location.href = "/index.html";
+        window.location.href = "/lobby.html";
       }
     },
     error: function (xhr, status, err) {
@@ -88,7 +64,7 @@ function doRegister() {
         error(res.message);
       } else {
         $.cookie("shSession", res.data.session, {path: '/', expires: 365});
-        window.location.href = "/index.html";
+        window.location.href = "/lobby.html";
       }
     },
     error: function (xhr, status, err) {
@@ -123,7 +99,7 @@ function doAnonymous() {
         }
       } else {
         $.cookie("shSession", res.data.session, {path: '/', expires: 365});
-        window.location.href = "/index.html";
+        window.location.href = "/lobby.html";
       }
     },
     error: function (xhr, status, err) {
