978ebb475bd161a7689296a0882cb26c7e1145b5
Scott Dale <scott@lgdales.com>
2013-05-30 13:22:28 +1000

    must reset the dnaName change dependent params;

--- config/ip-10-174-177-87.js
+++ config/ip-10-174-177-87.js
@@ -1,6 +1,11 @@
 var config = exports;
 
 config.dnsName = "dev2.skool51.com";
+// must reset these and dnsName has changed
+config.adminUrl = "http://" + config.dnsName + ":" + config.adminPort;
+config.restUrl = "http://" + config.dnsName + ":" + config.restPort + "/api";
+config.gamesUrl = "http://" + config.dnsName + ":" + config.gamesPort;
+config.socketUrl = "ws://" + config.dnsName + ":" + config.socketPort;
 
 // db
 config.db = {};
