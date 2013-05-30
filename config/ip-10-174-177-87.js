64b86cf31f4f81f491823f2f84b7411816f5f1c5
Scott Dale <scott@lgdales.com>
2013-05-30 13:33:16 +1000

    add back the ports as they are undefined in this sub config;

--- config/ip-10-174-177-87.js
+++ config/ip-10-174-177-87.js
@@ -1,10 +1,17 @@
 var config = exports;
 
 config.dnsName = "dev2.skool51.com";
-// must reset these and dnsName has changed
+
+config.adminPort = 5100;
 config.adminUrl = "http://" + config.dnsName + ":" + config.adminPort;
+
+config.restPort = 5101;
 config.restUrl = "http://" + config.dnsName + ":" + config.restPort + "/api";
+
+config.gamesPort = 5102;
 config.gamesUrl = "http://" + config.dnsName + ":" + config.gamesPort;
+
+config.socketPort = 5110;
 config.socketUrl = "ws://" + config.dnsName + ":" + config.socketPort;
 
 // db
