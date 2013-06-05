8ce35f68172481518cc43a3e26f57b4414f5f05d
Scott Dale <scott@lgdales.com>
2013-06-05 17:19:22 +1000

    add popOrPush for the match queue;

--- src/db/shsqlite.js
+++ src/db/shsqlite.js
@@ -44,6 +44,58 @@ shSqlite.del = function (key, cb) {
   this.client.run("DELETE FROM store WHERE key = ?", key, cb);
 };
 
+shSqlite.popOrPush = function (queueName, minMatches, data, cb) {
+  var self = this;
+  this.client.serialize(function () {
+    self.client.run("BEGIN");
+    self.get(queueName, function (err, row) {
+      if (row === null) {
+        // nothing in queue - set it
+        self.set(queueName, JSON.stringify([data]), function (err) {
+          console.log("set", queueName, err);
+          self.client.run("COMMIT", function (err) {
+            cb(0, null);
+          });
+        });
+        return;
+      }
+      var info = JSON.parse(row);
+
+      // check user already in queue
+      console.log(info);
+      var me = _.first(info, function (user) {
+        return user.uid === data.uid;
+      });
+      if (me.length !== 0) {
+        console.log("user queued already", queueName, data.uid, me);
+        self.client.run("COMMIT", function (err) {
+          return cb(2, null);
+        });
+        return;
+      }
+      // check min matches against current list + me
+      if (info.length + 1 < minMatches) {
+        info.push(data);
+        self.set(queueName, JSON.stringify(info), function (err) {
+          console.log("add user to existing queue", queueName, err);
+          self.client.run("COMMIT", function (err) {
+            cb(0, null);
+          });
+        });
+        return;
+      }
+
+      // match made - send list back
+      self.del(queueName, function (err) {
+        console.log("clear queue", queueName, err);
+        self.client.run("COMMIT", function (err) {
+          cb(0, info);
+        });
+      });
+    });
+  });
+}
+
 shSqlite.close = function (cb) {
   this.client.close(cb);
 };
