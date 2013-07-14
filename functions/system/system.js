2674e8454bf8442fc36242134808c8903e94143e
Scott Dale <scott@lgdales.com>
2013-07-14 14:10:18 +1000

    spell out CONFIG;

--- functions/system/system.js
+++ functions/system/system.js
@@ -45,7 +45,7 @@ system.stats = function (req, res, cb) {
 system.config = function (req, res, cb) {
   shlog.info("system.config");
 
-  res.add(sh.event("system.config", {gBaseDir: global.gBaseDir, CONF: global.C, PACKAGE: global.PACKAGE}));
+  res.add(sh.event("system.config", {gBaseDir: global.gBaseDir, CONFIG: global.C, PACKAGE: global.PACKAGE}));
   return cb(0);
 };
 
