a1297c4ef9ac44500993d59030a8455b0af2cbfb
Scott Dale <scott@lgdales.com>
2013-08-19 16:54:20 +1000

    add example.js api; fix up the game over div;

---
+++ apis/example/example.js
@@ -0,0 +1,19 @@
+var sh = require(global.C.BASEDIR + "/src/shutil.js");
+
+var example = exports;
+
+example.desc = "utility functions for shelly modules";
+example.functions = {
+  hello: {desc: "simple hello world api example", params: {}, security: []},
+  echo: {desc: "one parameter api example", params: {param1: {dtype: "string"}}, security: []}
+};
+
+example.hello = function (req, res, cb) {
+  res.add(sh.event("example.hello", "world"));
+  return cb(0);
+};
+
+example.echo = function (req, res, cb) {
+  res.add(sh.event("example.echo", req.body.param1));
+  return cb(0);
+};
