2a1c6494c8b365509dee0304fcfc7773ee994ad8
Scott Dale <scott@lgdales.com>
2013-08-23 13:51:46 +1000

    full path the db wrapper;

--- config/ip-10-174-177-87.js
+++ config/ip-10-174-177-87.js
@@ -3,5 +3,5 @@
 global.CDEF("DNS_NAME", "dev2.skool51.com");
 
 // db
-global.CDEF("DB_WRAPPER", "/lib/db/shredis.js");
+global.CDEF("DB_WRAPPER", global.C.BASEDIR + "/lib/db/shredis.js");
 global.CDEF("DB_OPTIONS", {});
