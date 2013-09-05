7d7a921ae24c1ce4e585d3e31b70e2c8e9915353
Scott Dale <scott@lgdales.com>
2013-09-05 14:59:53 +1000

    add control options for simple file and console logging; add a global logHook function to set any other options desired;

--- config/main.js
+++ config/main.js
@@ -39,9 +39,14 @@ global.CDEF("CLUSTER", {
 });
 global.CDEF("CLUSTER_AUTO_GAME_MATCHER", true); // auto adds matcher processes per game
 
-// logs
+// logs: level = system, error, info, debug
+global.CDEF("LOG_CONSOLE", true);
 global.CDEF("LOG_CONSOLE_OPTS", { level: "info", colorize: true, timestamp: false });
-//global.CDEF("LOG_CONSOLE_OPTS", { level: "debug", colorize: true, timestamp: false });
+global.CDEF("LOG_FILE", false);
+global.CDEF("LOG_FILE_OPTS", { level: "info", json: false, timestamp: true, filename: global.C.BASEDIR + "/logs/shelly.log" });
+//global.logHook = function (winston) {
+//  console.log("add to winston");
+//};
 
 // registration
 global.CDEF("REG_ALLOW_ANONYMOUS", true);
