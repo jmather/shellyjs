19e1137d8f9294facabf50df47c43bb6f1ccd299
Scott Dale <scott@lgdales.com>
2013-09-11 17:26:56 +1000

    add shellyh.js to clean up login and start using shelly client;

---
+++ www/common/shellyh.js
@@ -0,0 +1,45 @@
+
+function Shelly(restUrl) {
+  this.restUrl = restUrl;
+  this.token = "";
+}
+
+Shelly.prototype.checkToken = function (newToken) {
+  this.token = $.cookie("shToken");
+  if (typeof (this.token) === "undefined") {
+    this.token = newToken;
+    $.cookie("shToken", this.token, { expires: 3650, path: "/" });
+  }
+};
+
+Shelly.prototype.login = function (email, password, role, cb) {
+  var data = {cmd: "reg.login",
+    email: email,
+    password: password,
+    role: role
+  };
+
+  this.call(data, function (res, status) {
+    if (res[0].event === "reg.login") {
+      $.cookie("shSession", res[0].data.session, {path: "/", expires: 365});
+    }
+    cb(res, status);
+  });
+};
+
+Shelly.prototype.call = function (data, cb) {
+  $.ajax ({
+    type: "POST",
+    url: Env.restUrl,
+    async: false,
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify(data),
+    success: cb,
+    error: function (xhr, status, err) {
+      error(err);
+    }
+  });
+};
+
+var shelly = new Shelly();
