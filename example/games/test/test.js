6ded0fdd76b0b774a8ffbe6446cb7758a59f4b3b
Scott Dale <scott@lgdales.com>
2013-10-22 18:26:41 +1100

    enable simple guess app to test > 2 players;

--- example/games/test/test.js
+++ example/games/test/test.js
@@ -8,26 +8,36 @@ test.url = "/test/test.html";
 test.enabled = false;
 
 test.config = {
-  enabled: false,
+  enabled: true,
   url: "/test/test.html",
   minPlayers: 3,
   maxPlayers: 4
 };
 
-test.create = function (req, cb) {
-  req.env.game.state = {number: _.random(10)};
+test.create = function (req, res, cb) {
+  req.env.game.set("state", {number: _.random(10), winner: false});
 
   cb(0); // by default game.create returns game object
 };
 
-test.turn = function (req, cb) {
-  if (req.body.guess === req.env.game.state.number) {
-    cb(0, sh.event("test.info", {message: "you won"}));
-  } else {
-    cb(0, sh.event("test.info", {message: "try again"}));
-  }
+test.reset = function (req, res, cb) {
+  req.env.game.set("state", {number: _.random(10), winner: false});
+
+  cb(0);
 };
 
-test.myfunc = function (req, cb) {
+test.turn = function (req, res, cb) {
+  var state = req.env.game.get("state");
+  state.lastId = req.session.uid;
+  state.guess = req.body.guess;
+
+  state.winner = parseInt(req.body.guess, 10) === state.number;
+  if (state.winner) {
+    req.env.game.set("status", "over");
+  }
+  cb(0, {lastId: req.session.uid, winner: state.winner, guess: req.body.guess});
+};
+
+test.myfunc = function (req, res, cb) {
   cb(0, sh.event("test.myfunc", {message: "hellow world"}));
 };
